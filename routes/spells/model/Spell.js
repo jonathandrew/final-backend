0ec0da8ef2c6c1f91774308230339a0a0412dda1
Jonathan Drew <jonathan.drew@codeimmersives.com>
2020-04-23 10:25:39 -0400

    need to make spells click with db to add more with button created

--- routes/spells/model/Spell.js
+++ routes/spells/model/Spell.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const SpellSchema = new mongoose.Schema({
   spell: { type: String },
   effect: { type: String },
+  test: { type: String },
 });
 
 module.exports = mongoose.model("Spell", SpellSchema);
